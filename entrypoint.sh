d0ae48dad2ab5a5934f9156dce68f0fc2413bf7d
Ian Kidd <45671561+ikidd@users.noreply.github.com>
2022-01-24 00:53:23 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@ wg-quick up wg0
 chmod u+x /opt/wgdashboard/wgd.sh
 if [ ! -f "/opt/wgdashboard/wg-dashboard.ini" ]; then
   /opt/wgdashboard/wgd.sh install
-  /opt/wgdashboard/wgd.sh start
-  /opt/wgdashboard/wgd.sh stop
+#  /opt/wgdashboard/wgd.sh start
+#  /opt/wgdashboard/wgd.sh stop
 fi
-/opt/wgdashboard/wgd.sh debug
+/opt/wgdashboard/wgd.sh start
 sleep infinity
